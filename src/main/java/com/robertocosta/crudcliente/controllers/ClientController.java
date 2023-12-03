e3c01ade47de26699d8f3d0fd56a412b36555a04
Roberto Costa <robertocosta66@gmail.com>
2023-12-03 00:16:00 -0300

    New Client

--- src/main/java/com/robertocosta/crudcliente/controllers/ClientController.java
+++ src/main/java/com/robertocosta/crudcliente/controllers/ClientController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +24,15 @@ public class ClientController {
 	public Page<ClientDTO> findAll(Pageable pageable) {
 		return service.findAll(pageable);
 	}
+	
+	@GetMapping(value = "/{id}")
+	public ClientDTO finBiId(@PathVariable Long id) {
+		return service.findById(id);
+	}
+	
+	@PostMapping
+	public ClientDTO insert(@RequestBody ClientDTO dto) {
+		return service.insert(dto);
+	}
+
 }
--- src/main/java/com/robertocosta/crudcliente/service/ClientService.java
+++ src/main/java/com/robertocosta/crudcliente/service/ClientService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.robertocosta.crudcliente.dto.ClientDTO;
+import com.robertocosta.crudcliente.entities.Client;
 import com.robertocosta.crudcliente.repositories.ClientRepository;
 
 @Service
@@ -18,6 +19,23 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public Page<ClientDTO> findAll(Pageable pageable){
 		return repository.findAll(pageable).map(x -> new ClientDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		return new ClientDTO(repository.findById(id).get());
+	}
+	
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+		
+		entity  = repository.save(entity);
+		return new ClientDTO(entity);
 	}
 
 }
