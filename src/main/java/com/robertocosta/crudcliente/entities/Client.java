a37342d08d9966cabfd72bf45cd12af5e5cc6739
Roberto Costa <robertocosta66@gmail.com>
2023-12-02 22:53:44 -0300

    Seeding da base de dados

--- src/main/java/com/robertocosta/crudcliente/entities/Client.java
+++ src/main/java/com/robertocosta/crudcliente/entities/Client.java
@@ -3,6 +3,7 @@ package com.robertocosta.crudcliente.entities;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,8 @@ public class Client {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(unique = true)
 	private String cpf;
 	private Double income;
 	private LocalDate birthDate;
