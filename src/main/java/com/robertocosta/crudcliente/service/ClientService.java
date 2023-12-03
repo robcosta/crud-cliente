0c1021f95c2637601a5ac045484669f8c5d331df
Roberto Costa <robertocosta66@gmail.com>
2023-12-03 00:45:00 -0300

    Atualizando Client com PUT

--- src/main/java/com/robertocosta/crudcliente/service/ClientService.java
+++ src/main/java/com/robertocosta/crudcliente/service/ClientService.java
@@ -26,16 +26,30 @@ public class ClientService {
 		return new ClientDTO(repository.findById(id).get());
 	}
 	
+	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
-		Client entity = new Client();
+		Client entity = new Client();		
+		copyDtoToEntity(dto, entity);		
+		entity  = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		Client entity = repository.getReferenceById(id);;
+		copyDtoToEntity(dto, entity);		
+		entity  = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+		
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
-		entity.setChildren(dto.getChildren());
-		
-		entity  = repository.save(entity);
-		return new ClientDTO(entity);
+		entity.setChildren(dto.getChildren());		
 	}
+
+	
 
 }
